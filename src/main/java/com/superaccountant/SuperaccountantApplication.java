6cd370cdc7a143c105e4e2aa93ab9acf59dcb5a4
Ram Kumar <ramgambit@gmail.com>
2025-08-30 18:52:26 +0530

    initial project setup for super accountant

---
+++ src/main/java/com/superaccountant/SuperaccountantApplication.java
@@ -0,0 +1,13 @@
+package com.superaccountant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperaccountantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperaccountantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superaccountant/superaccountant/SuperaccountantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superaccountant.superaccountant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperaccountantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
